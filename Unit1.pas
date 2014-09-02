f9afadbad0b044984148d8740a84d889eb1a2be4
José Pérez <jmperez27@gmail.com>
2014-09-02 10:36:00 +0200

    +Add new button

--- Unit1.pas
+++ Unit1.pas
@@ -4,10 +4,12 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls;
 
 type
   TForm1 = class(TForm)
+    Button1: TButton;
+    procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +22,10 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TForm1.Button1Click(Sender: TObject);
+begin
+  ShowMessage('Hello World!');
+end;
 
 end.
